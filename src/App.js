fe28cd06a0bdac88270571082c0af2038212cd2d
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-06-04 20:29:25 +0200

    linear styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    active: false,
+  };
+  render() {
+    // pierwszy sposób
+
+    // const btn_off = {
+    //   border: "2px solid black",
+    //   padding: "10px 20px",
+    //   fontFamily: "arial",
+    //   fontSize: 30,
+    //   display: "block",
+    //   margin: "20px auto",
+    //   backgroundColor: "white",
+    // };
+
+    // const btn_on = {
+    //   border: "2px solid black",
+    //   padding: "10px 20px",
+    //   fontFamily: "arial",
+    //   fontSize: 30,
+    //   display: "block",
+    //   margin: "20px auto",
+    //   backgroundColor: "cadetblue",
+    //   color: "white",
+    // };
+
+    // drugi sposób
+
+    // const styles = {
+    //   btn_off: {
+    //     border: "2px solid black",
+    //     padding: "10px 20px",
+    //     fontFamily: "arial",
+    //     fontSize: 30,
+    //     display: "block",
+    //     margin: "20px auto",
+    //     backgroundColor: "white",
+    //   },
+    //   btn_on: {
+    //     border: "2px solid black",
+    //     padding: "10px 20px",
+    //     fontFamily: "arial",
+    //     fontSize: 30,
+    //     display: "block",
+    //     margin: "20px auto",
+    //     backgroundColor: "cadetblue",
+    //     color: "white",
+    //   },
+    // };
+
+    // sposób trzeci
+
+    let btn = {
+      border: "2px solid black",
+      padding: "10px 20px",
+      fontFamily: "arial",
+      fontSize: 30,
+      display: "block",
+      margin: "20px auto",
+      backgroundColor: "white",
+    };
+
+    if (this.state.active) {
+      btn.backgroundColor = "cadetblue";
+      btn.color = "white";
+    }
+    return (
+      <div>
+        <button
+          style={btn}
+          onClick={() => this.setState({ active: !this.state.active })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {this.state.active ? "Wyłącz" : "Włącz"}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
